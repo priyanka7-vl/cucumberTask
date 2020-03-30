4af424c424f2b29c83611836b90140c81a8dda2a
priyanka <priyanka.vlr@rapidops.com>
2020-03-30 19:50:15 +0530

    settings page added

--- features/salesmateLogin.feature
+++ features/salesmateLogin.feature
@@ -2,5 +2,6 @@ Feature: salesmate Login Page
          To enter the credentials for salesmate login page
 Scenario: Entering credentials
    Given I am on the salesmate login page
-   When I enter the credentials
-   Then the page salesmate homescreen page should be opened
+   When I enter the valid credentials
+   When I go to the general settings page
+   Then the browser title should be matched with the page opened
--- features/step_definitions/salesmateLogin_steps.js
+++ features/step_definitions/salesmateLogin_steps.js
@@ -1,25 +1,54 @@
 const { Given, When, Then,AfterAll} = require('cucumber');
-const { Builder, By, Capabilities} = require('selenium-webdriver');
+const { Builder, By,Key, Capabilities} = require('selenium-webdriver');
 const {strictEqual} = require('assert');
 require("chromedriver");
 const capabilities = Capabilities.chrome();
 capabilities.set('chromeOptions', { "w3c": false });
 const driver = new Builder().withCapabilities(capabilities).build();
 
-Given('I am on the salesmate login page', {timeout: 2 * 8000},async function () {
+Given('I am on the salesmate login page', {timeout: 2 * 9000},async function () {
     await driver.get('https://accounts.salesmate.io/login.html#');
 });
 
-When('I enter the credentials', async function () {
+When('I enter the valid credentials',{timeout: 2 * 30000}, async function () {
     await driver.findElement(By.xpath("//input[@id='email']")).sendKeys("priyanka.vlr@rapidops.com");
     await driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
     await driver.findElement(By.xpath("//button[@id='login_btn']")).click();
+    await driver.sleep(18000);
+    await driver.findElement(By.xpath("//span[@class='thumb-sm avatar avatar-6']")).click();
+    await driver.findElement(By.xpath("//a[text()='My Account']")).click();
+    await driver.sleep(10000);  
+});
+When('I go to the general settings page', async function () { 
+    await driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys(Key.chord(Key.CONTROL,"a"));
+    await driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys(Key.chord(Key.DELETE));
+    await driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys(Key.chord(Key.CONTROL,"a"));
+    await driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys(Key.chord(Key.DELETE));
+    await driver.findElement(By.xpath("//input[@id='email']")).sendKeys(Key.chord(Key.CONTROL,"a"));
+    await driver.findElement(By.xpath("//input[@id='email']")).sendKeys(Key.chord(Key.DELETE));
+    await driver.findElement(By.xpath("//button[@id ='btnUpdate']")).click();
+    let header2 = await driver.findElement(By.xpath("//div[@class='error-message text-danger']")).getText();
+    strictEqual(header2,'This field is required.');
+    await driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("priyanka");
+    await driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("vlr");
+    await driver.findElement(By.xpath("//input[@id='email']")).sendKeys("priyanka.vlr@rapidops.com");
+    await driver.findElement(By.xpath("//input[@id='mobile']")).sendKeys(Key.chord(Key.CONTROL,"a"));
+    await driver.findElement(By.xpath("//input[@id='mobile']")).sendKeys(Key.chord(Key.DELETE));
+    await driver.findElement(By.xpath("//input[@id='mobile']")).sendKeys("9553387165");
+    await driver.findElement(By.xpath("//span[@role='presentation']")).click();
+    await driver.findElement(By.xpath("//li[text()='24 Hour']")).click();
+    await driver.findElement(By.xpath("//span[@title='MMM DD, YYYY']")).click();
+    await driver.findElement(By.xpath("//li[text()='DD MMM, YYYY']")).click();
+    await driver.findElement(By.xpath("//span[@title='Asia/Kolkata']")).click();
+    await driver.findElement(By.xpath("//input[@id='nickname']")).sendKeys(Key.chord(Key.CONTROL,"a"));
+    await driver.findElement(By.xpath("//input[@id='nickname']")).sendKeys(Key.chord(Key.DELETE));
+    await driver.findElement(By.xpath("//input[@id='nickname']")).sendKeys("priya");
+    await driver.findElement(By.xpath("//button[@id ='btnUpdate']")).click();
 });
 
-Then('the page salesmate homescreen page should be opened',{timeout: 2 * 12000},async function () {
-    await driver.sleep(20000);
+Then('the browser title should be matched with the page opened',async function () {
     const title = await driver.getTitle();
-    const expectedTitle = 'Default Dashboard';
+    const expectedTitle = 'General - My Account';
     strictEqual(title,expectedTitle);
 });
 
